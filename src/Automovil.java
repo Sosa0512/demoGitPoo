9eaf3e2fbe8b3722638f20a081db3babde2809a3
samuelgomezs134 <samuelgomezs134@gmail.com>
2024-10-31 08:35:06 -0500

    Se genera automovil

---
+++ src/Automovil.java
@@ -0,0 +1,11 @@
+public class Automovil extends Vehiculo{
+    private boolean descapotable;
+
+    public boolean isDescapotable() {
+        return descapotable;
+    }
+
+    public void setDescapotable(boolean descapotable) {
+        this.descapotable = descapotable;
+    }
+}
