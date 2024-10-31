b51c41c0a0145676eb7a041a0920b80207ee8ef6
samuelgomezs134 <samuelgomezs134@gmail.com>
2024-10-31 08:21:43 -0500

    Se crea vehiculo

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Estamos practicando SCV");
+        String name = "Jhon Doe";
+        int age = 25;
+        boolean status = true;
     }
 }
